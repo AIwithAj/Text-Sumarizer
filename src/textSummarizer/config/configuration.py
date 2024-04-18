680a5b63464e095ead7e36f96f4e961e3398302a
Ajay <dancerworld60@gmail.com>
2024-04-18 20:09:49 +0530

    model evaluation done

--- src/textSummarizer/config/configuration.py
+++ src/textSummarizer/config/configuration.py
@@ -1,6 +1,10 @@
 from textSummarizer.constants import *
 from textSummarizer.utils.common import read_yaml, create_directories
-from src.textSummarizer.entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig,ModelTrainerConfig
+from textSummarizer.entity import (DataIngestionConfig,
+                                   DataValidationConfig,
+                                   DataTransformationConfig,
+                                   ModelTrainerConfig,
+                                   ModelEvaluationConfig)
 
 
 class ConfigurationManager:
@@ -31,6 +35,7 @@ class ConfigurationManager:
         return data_ingestion_config
     
 
+
     def get_data_validation_config(self) -> DataValidationConfig:
         config = self.config.data_validation
 
@@ -59,7 +64,8 @@ class ConfigurationManager:
         return data_transformation_config
     
 
- def get_model_trainer_config(self) -> ModelTrainerConfig:
+
+    def get_model_trainer_config(self) -> ModelTrainerConfig:
         config = self.config.model_trainer
         params = self.params.TrainingArguments
 
@@ -83,4 +89,18 @@ class ConfigurationManager:
         return model_trainer_config
     
 
-    
+    def get_model_evaluation_config(self) -> ModelEvaluationConfig:
+        config = self.config.model_evaluation
+
+        create_directories([config.root_dir])
+
+        model_evaluation_config = ModelEvaluationConfig(
+            root_dir=config.root_dir,
+            data_path=config.data_path,
+            model_path = config.model_path,
+            tokenizer_path = config.tokenizer_path,
+            metric_file_name = config.metric_file_name
+           
+        )
+
+        return model_evaluation_config
--- src/textSummarizer/entity/__init__.py
+++ src/textSummarizer/entity/__init__.py
@@ -9,17 +9,21 @@ class DataIngestionConfig:
     unzip_dir: Path
 
 
+
 @dataclass(frozen=True)
 class DataValidationConfig:
     root_dir: Path
     STATUS_FILE: str
     ALL_REQUIRED_FILES: list
 
+
+
 @dataclass(frozen=True)
 class DataTransformationConfig:
     root_dir: Path
     data_path: Path
     tokenizer_name: Path
+
 
 
 @dataclass(frozen=True)
@@ -38,3 +42,11 @@ class ModelTrainerConfig:
     gradient_accumulation_steps: int
 
 
+
+@dataclass(frozen=True)
+class ModelEvaluationConfig:
+    root_dir: Path
+    data_path: Path
+    model_path: Path
+    tokenizer_path: Path
+    metric_file_name: Path
